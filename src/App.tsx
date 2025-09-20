384b57bd5e1c68355d3b0d01c5883d84f516eee6
SYAFIEQAH BINTI AHMAD SHUKRI <TP084978@mail.apu.edu.my>
2025-09-21 00:49:30 +0800

    duplicate chatbot fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import AlertSystem from './components/AlertSystem'
 import RouteOptimizer from './components/RouteOptimizer'
 import UserProfile from './components/UserProfile'
 import Login from './components/Login'
-import ChatBot from './components/ChatBot'
 import LexChatBot from './components/LexChatBot'
 import Footer from './components/Footer'
 
@@ -38,9 +37,7 @@ function App() {
                   <Route path="/routes" element={<RouteOptimizer />} />
                   <Route path="/profile" element={<UserProfile />} />
                 </Routes>
-                {/* Basic AI Assistant (left side) */}
-                <ChatBot />
-                {/* Amazon Lex Assistant (right side) */}
+                {/* Amazon Lex Assistant - Single chatbot only */}
                 <LexChatBot />
                 <Footer />
               </>
